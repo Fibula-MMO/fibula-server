55b3793d518a860a2eccbe2d6c2c65a5ae6a19c5
Sean Owen <sean_owen@live.co.uk>
2020-09-21 23:12:55 +0100

    Add palyer mode packet and corresponding packet writer - packet writer currently unfinished

---
+++ src/Fibula.Protocol.V772/PacketWriters/PlayerModePacketWriter.cs
@@ -0,0 +1,47 @@
+﻿// -----------------------------------------------------------------
+// <copyright file="PlayerModePacketWriter.cs" company="2Dudes">
+// Copyright (c) | Jose L. Nunez de Caceres et al.
+// https://linkedin.com/in/nunezdecaceres
+//
+// All Rights Reserved.
+//
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// </copyright>
+// -----------------------------------------------------------------
+
+namespace Fibula.Protocol.V772.PacketWriters
+{
+    using Fibula.Communications;
+    using Fibula.Communications.Contracts.Abstractions;
+    using Fibula.Communications.Packets.Outgoing;
+    using Fibula.Protocol.V772.Extensions;
+    using Serilog;
+
+    /// <summary>
+    /// Class that represents a player mode packet writer for the game server.
+    /// </summary>
+    public class PlayerModePacketWriter : BasePacketWriter
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PlayerModePacketWriter"/> class.
+        /// </summary>
+        /// <param name="logger">A reference to the logger in use.</param>
+        public PlayerModePacketWriter(ILogger logger)
+            : base(logger)
+        {
+        }
+
+        /// <inheritdoc/>
+        public override void WriteToMessage(IOutboundPacket packet, ref INetworkMessage message)
+        {
+            if (!(packet is PlayerModePacket playerModePacket))
+            {
+                this.Logger.Warning($"Invalid packet {packet.GetType().Name} routed to {this.GetType().Name}");
+
+                return;
+            }
+
+            message.AddByte(playerModePacket.PacketType.ToByte());
+        }
+    }
+}
